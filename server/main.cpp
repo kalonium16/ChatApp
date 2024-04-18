4d206e280f9f26ea0844e53638e09eaca334ebee
Anton Latanskyy <52884811+kalonium16@users.noreply.github.com>
2024-04-18 10:10:09 +0300

    Update main.cpp

--- server/main.cpp
+++ server/main.cpp
@@ -1,12 +1,22 @@
-
+#include <iostream>
 #include "server.h"
 
 
 int main(int argc, char* argv[]) {
 	int port{6000};
 	if (argc >= 2) {
-		port = std::stoi(argv[1]);
-		
+		std::string portString{ argv[1] };
+		auto err = std::from_chars(portString.data(), portString.data() + portString.size(), port).ec;
+		if (err == std::errc::invalid_argument || err == std::errc::result_out_of_range) {
+			std::cout << "Wrong port try again";
+			return 0;
+		}
+		else {
+			if (port < 0 && port > 65535) {
+				std::cout << "Wrong port try again";
+				return 0;
+			}
+		}
 	}
 	Server server{ port };
 	server.start();
