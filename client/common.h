883f960f3c27c5456eb0787c7c0eaad40e4ee1c3
Anton Latanskyy <52884811+kalonium16@users.noreply.github.com>
2024-04-19 05:54:26 +0300

    Update common.h

--- client/common.h
+++ client/common.h
@@ -3,7 +3,7 @@
 #include <memory>
 struct messagingData
 {
-	std::atomic_bool waitingForMessage = false;
+	std::atomic_bool waitingForMessage{ false };
 	std::atomic_bool isOnline{ false };
 	std::atomic<uint32_t> userId{};
 };
